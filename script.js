50d6bffed4745bfb6dad7681e2149b86712dcb92
Cosmolog1 <bourret57@gmail.com>
2025-01-13 16:25:46 +0100

    Cv_Ligne_v0.5

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const display = document.querySelector("#display")
 async function fetchGitHubProfile(username) {
     try {
         const response = await fetch(`https://api.github.com/users/${username}`);
-
-
-
         if (!response.ok) {
             throw new Error(`GitHub user not found: ${response.statusText}`);
         }
